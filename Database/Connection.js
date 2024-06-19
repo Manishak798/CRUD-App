d32edb359c187854749d3c7b0ba8f5b327f52963
Manisha Kundnani <90680330+Manishak798@users.noreply.github.com>
2024-06-19 22:47:27 +0530

    Update Connection.js

--- Database/Connection.js
+++ Database/Connection.js
@@ -1,7 +1,7 @@
 import { mongodbURI } from "../config.js";
 import mongoose from "mongoose";
 
-
+//connecting to DB
 
 export const ConnectionDB = mongoose.connect(mongodbURI).then(() => {
     console.log('MongoDB Connected');
